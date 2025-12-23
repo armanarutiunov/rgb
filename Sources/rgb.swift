7646919b887d8a2174f93d134d8783f6c6305d49
arman <arman.arutiunov@gmail.com>
2025-12-23 23:39:08 +0000

    Hello world

---
+++ Sources/rgb.swift
@@ -0,0 +1,11 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import ArgumentParser
+
+@main
+struct rgb {
+    static func main() {
+        print("Hello, world!")
+    }
+}
