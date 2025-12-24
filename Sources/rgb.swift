cbb5d0b9b54d85d267444271c4707aa53db5f159
arman <arman.arutiunov@gmail.com>
2025-12-24 00:12:38 +0000

    ToHex command

--- Sources/rgb.swift
+++ Sources/rgb.swift
@@ -1,11 +1,37 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
 import ArgumentParser
+import CoreGraphics
 
 @main
-struct rgb {
-    static func main() {
-        print("Hello, world!")
+struct RGB: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "An RGB converter tool. Use it to convert RGB(A) to hex and vica versa.",
+        subcommands: [ToHex.self]
+    )
+}
+
+struct ToHex: ParsableCommand {
+    static let configuration = CommandConfiguration(abstract: "Parse an RGB value to hex")
+
+    @Argument(help: "Red")
+    var r: Float
+
+    @Argument(help: "Green")
+    var g: Float
+
+    @Argument(help: "Blue")
+    var b: Float
+
+    @Option(name: .short, help: "Alpha")
+    var a: Float = 1
+
+    func run() {
+        func clamp01(_ x: CGFloat) -> CGFloat {
+            min(max(x, 0), 255)
+        }
+        let r8 = Int(round(clamp01(CGFloat(r))))
+        let g8 = Int(round(clamp01(CGFloat(g))))
+        let b8 = Int(round(clamp01(CGFloat(b))))
+        let a8 = Int(round(clamp01(CGFloat(a))))
+        print(String(format: "#%02X%02X%02X%02X", r8, g8, b8, a8))
     }
 }
